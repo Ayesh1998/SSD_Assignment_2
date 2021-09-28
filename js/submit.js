2f87b29e765af27e29d46a0a54b77e2fd048595b
Adeesha97 <adeeshakawshal1997@gmail.com>
2021-09-28 21:57:06 +0530

    Ajax Aou

--- js/submit.js
+++ js/submit.js
@@ -9,3 +9,27 @@ $(document).ready(function(){
     var access_token= "";
     var client_id = "34840218804-6tu4spu6a7121pk4gbt09tkbb466ecfa.apps.googleusercontent.com";// replace it with your client id;
 
+
+    $.ajax({
+        type: 'POST',
+        url: "https://www.googleapis.com/oauth2/v4/token",
+        data: {code:code
+            ,redirect_uri:redirect_uri,
+            client_secret:client_secret,
+            client_id:client_id,
+            scope:scope,
+            grant_type:"authorization_code"},
+        dataType: "json",
+        success: function(resultData) {
+
+
+            localStorage.setItem("accessToken",resultData.access_token);
+            localStorage.setItem("refreshToken",resultData.refreshToken);
+            localStorage.setItem("expires_in",resultData.expires_in);
+            // window.history.pushState({}, document.title, "/DriveSubmIt/modules/" + "submit.html");
+
+            window.history.pushState({}, document.title, "" + "submit.html");
+
+
+        }
+    });
