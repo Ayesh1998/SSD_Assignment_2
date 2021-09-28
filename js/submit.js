2ce4e25e6cc4f9cf0516ec59483753b0921e582f
Adeesha97 <adeeshakawshal1997@gmail.com>
2021-09-28 21:56:19 +0530

    variable dec

--- js/submit.js
+++ js/submit.js
@@ -4,3 +4,8 @@ $(document).ready(function(){
     const code = urlParams.get('code');
     const redirect_uri = "http://192.168.64.2.xip.io/DriveSubmIt/modules/submit.html"; // replace with your redirect_uri;
 
+    const client_secret = "HxtwKpr33ktGhuOFpRQUI6G1"; // replace with your client secret
+    const scope = "https://www.googleapis.com/auth/drive";
+    var access_token= "";
+    var client_id = "34840218804-6tu4spu6a7121pk4gbt09tkbb466ecfa.apps.googleusercontent.com";// replace it with your client id;
+
