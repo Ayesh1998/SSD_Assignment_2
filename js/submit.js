6c2e04a58bb8a57a194c86babf474e904290d7b5
Adeesha97 <adeeshakawshal1997@gmail.com>
2021-09-28 21:55:51 +0530

    Function Dec

--- js/submit.js
+++ js/submit.js
@@ -1,2 +1,6 @@
 $(document).ready(function(){
 
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    const redirect_uri = "http://192.168.64.2.xip.io/DriveSubmIt/modules/submit.html"; // replace with your redirect_uri;
+
