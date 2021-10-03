78eea486bae835bd55521f50086d307e81dfa3e4
Ayesh Lakshan <ayeshlak1998@gmail.com>
2021-10-03 20:08:04 +0530

    Readded keys

--- configs/keys.js
+++ configs/keys.js
@@ -1,11 +1,11 @@
 //this is the key
-const KEYS = {
-    'googleOauth': {
-        'clientID': "786803973402-up4b0jeq0t6cfr2bhg27uncbjmsf0dof.apps.googleusercontent.com",
-        'clientSecret': "3gk9r5DIMMemkulXkGOpioXT",
-        'callback': '/auth/google/redirect'
-    },
-    "session_key": "secret123"
-}
-
-module.exports = KEYS
+// const KEYS = {
+//     'googleOauth': {
+//         'clientID': "786803973402-up4b0jeq0t6cfr2bhg27uncbjmsf0dof.apps.googleusercontent.com",
+//         'clientSecret': "3gk9r5DIMMemkulXkGOpioXT",
+//         'callback': '/auth/google/redirect'
+//     },
+//     "session_key": "secret123"
+// }
+//
+// module.exports = KEYS
