d1ee22de8a3dde55a4f4cee48ebbe98a10b0c41d
Ayesh Lakshan <ayeshlak1998@gmail.com>
2021-10-03 19:59:35 +0530

    Added keys

---
+++ configs/keys.js
@@ -0,0 +1,10 @@
+const KEYS = {
+    // 'googleOauth': {
+    //     'clientID': "786803973402-up4b0jeq0t6cfr2bhg27uncbjmsf0dof.apps.googleusercontent.com",
+    //     'clientSecret': "3gk9r5DIMMemkulXkGOpioXT",
+    //     'callback': '/auth/google/redirect'
+    // },
+    "session_key": "secret123"
+}
+
+module.exports = KEYS
